5b1ace809572733f6c79d57a8f3a5511b9f91989
theafricanvampire <tenexperiences@gmail.com>
2023-02-22 09:36:52 +0300

    C programming

---
+++ genesis.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* will it run haha */
+in main(int argc, char *argv[]) {
+	printf("hello boniface, welcome to coding using C language");
+	return (0);
+}
+
